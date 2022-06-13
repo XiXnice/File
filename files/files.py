c1f6d781984643e76e4e1beed0c9085b1fe06f6e
XiXnice <kuzminov399@yandex.ru>
2022-06-13 13:02:39 +0300

    Final one

--- files/files.py
+++ files/files.py
@@ -1,81 +1,21 @@
-FILES_ONE = 'files/1.txt'
-FILES_TWO = 'files/2.txt'
-FILES_THREE = 'files/3.txt'
-FILES_WORK = 'files/work.txt'
+from hashlib import new
+import os
 
-files_one = {}
-files_two = {}
-files_three = {}
+def compile_files(files_list):
+    data = {}
+    for file in files_list:
+        with open(file, encoding="utf-8") as f:
+            file_data = f.readlines()
+            data[len(file_data)] = (file, "".join(file_data))
 
-def  files_worker_one(FILES_ONE, mode: str = 'r'):
-    with open(FILES_ONE, mode, encoding='UTF-8') as files:
-        len = 0
-        f = []
-        for file in files.read().split('\n'):
-            f.append(file)
-            len += 1
+    data = dict(sorted(data.items()))
 
-        files_one[len] = f
-    return files_one
+    with open("files/work.txt", "w", encoding="utf-8") as new_file:
+        for key, value in data.items():
+            # new_file.write(f"{value[0]} \n")
+            new_file.write(f"{key} \n")
+            new_file.write(f"{value[1]} \n")
 
-def  files_worker_two(FILES_TWO, mode: str = 'r'):
-    with open(FILES_TWO, mode, encoding='UTF-8') as files:
-        len = 0
-        f = []
-        for file in files.read().split('\n'):
-            f.append(file)
-            len += 1
-
-        files_two[len] = f
-    return files_two
-
-def  files_worker_three(FILES_THREE, mode: str = 'r'):
-    with open(FILES_THREE, mode, encoding='UTF-8') as files:
-        len = 0
-        f = []
-        for file in files.read().split('\n'):
-            f.append(file)
-            len += 1
-
-        files_three[len] = f
-    return files_three
-
-def files_worker(file_name, mode: str = 'r'):
-    files_worker_one(FILES_ONE)
-    files_worker_two(FILES_TWO)
-    files_worker_three(FILES_THREE)
-    for number_one in files_one.keys():
-        break
-
-    for number_two in files_two.keys():
-        break
-
-    for number_three in files_three.keys():
-        break
-
-    with open(file_name, mode, encoding='UTF-8') as files:
-        if number_three >= number_two and number_three >= number_one:
-            if number_one >= number_two:
-                string_one = 0
-                string_two = 0
-                string_three = 0
-                for three in files_three.values():
-                    for item_three in three:
-                        string_three += 1
-                        files.write(f'{"Строка номер", string_three, "файла номер 3: ", item_three}\n')
-                    break
-                for one in files_one.values():
-                    for item_one in one:
-                        string_one += 1
-                        files.write(f'{"Строка номер", string_one, "файла номер 1: ", item_one}\n')
-                    break
-                for two in files_two.values():
-                    for item_two in two:
-                        string_two += 1
-                        files.write(f'{"Строка номер", string_two, "файла номер 2: ", item_two}\n')
-                    break
-
-        else:
-            print('Ошибка')
-
-files_worker(FILES_WORK, 'a')
+files = ["files/1.txt", "files/2.txt", "files/3.txt"]
+files = [os.path.join(os.getcwd(), file) for file in files]
+compile_files(files)
